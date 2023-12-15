04587f6e5c584179c38a2cb4e720582b2f89b50e
Jakub Kiko <jakub.kiko@gmail.com>
2023-12-15 20:13:07 +0100

    Update zastavka.py

--- zastavka.py
+++ zastavka.py
@@ -4,13 +4,15 @@ import codecs
 import unidecode
 import re
 
-index_file_start = "<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Zastavka</title><link rel='stylesheet' href='style.css'><script src='search.js' type='text/javascript'/></head><body><div id='header'><div class='input'><input placeholder='Hľadaj zastávku...' type='text' id='input'/><div id='searchclear' onmousedown='input.value='';'><b>x</b></div></div></div><ul><li><a href='https://imhd.sk/ba/online-zastavkova-tabula?theme=darkblue&st=%3B%3B%3B'><strong class='name'/>4 Najbližšie zastávky</a></li><!-- odkazy na zastavky -->"
-index_file_end = "</body></html>"
+index_html = ""
+
+index_file_start = "<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Zastavka</title><link rel='stylesheet' href='style.css'><script src='search.js' type='text/javascript'/></head><body><div id='header'><div class='input'><input placeholder='Hľadaj zastávku...' type='text' id='input'/><div id='searchclear' onmousedown='input.value='';'><b>x</b></div></div></div><ul><!-- odkazy na zastavky -->"
+index_file_end = "</ul></body></html>"
 
 url = "http://www.imhd.sk/ba/online-zastavkova-tabula?st="
 
 file1 = open("index_new.html", "w", encoding="utf-8")
-file1.write(index_file_start)
+index_html.join(index_file_start)
 
 i = 0
 
@@ -52,8 +54,12 @@ while i < 594:
         li = '<li><a href="' + link + '"><strong class="name">' + lowercase + '</strong>' + title \
              + '</a></li>'
 
-        file1.write(li)
+        index_html.join(li)
         print(li)
 
-file1.write(index_file_end)
+index_html.join(index_file_end)
+
+soup2 = BeautifulSoup(index_html, 'html.parser')
+
+file1.write(soup2.prettify())
 file1.close()
