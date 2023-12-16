ea4e1ed7b810302bba11318ccd8db681ccae0821
jakubkiko <jakub.kiko@gmail.com>
2023-12-16 19:16:26 +0100

    added trim to to name so it will remove spaces from strings

--- search.js
+++ search.js
@@ -5,7 +5,7 @@ input.onkeyup = function () {
     var lis = document.getElementsByTagName('li');
     for (var i = 0; i < lis.length; i++) {
         var name = lis[i].getElementsByClassName('name')[0].innerHTML;
-        if (name.toUpperCase().indexOf(filter) == 0) 
+        if (name.trim().toUpperCase().indexOf(filter) == 0) 
             lis[i].style.display = 'list-item';
         else
             lis[i].style.display = 'none';
