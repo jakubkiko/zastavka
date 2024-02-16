2d31807262e451de11d3f2ef49cdd23c80a88d1f
Jakub Kiko <jakub.kiko@gmail.com>
2024-02-16 09:59:38 +0100

    fixed not working clear icon  "X" and empty first entry

--- zastavka.py
+++ zastavka.py
@@ -4,7 +4,7 @@ import codecs
 import unidecode
 import re
 
-index_html = "<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Zastavka</title><link rel='stylesheet' href='style.css'><script src='search.js' type='text/javascript'/></head><body><div id='header'><div class='input'><input placeholder='Hľadaj zastávku...' type='text' id='input'/><div id='searchclear' onmousedown='input.value='';'><b>x</b></div></div></div><ul><!-- odkazy na zastavky -->"
+index_html = "<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Zastavka</title><link rel='stylesheet' href='style.css'><script src='search.js' type='text/javascript'/></head><body><div id='header'><div class='input'><input placeholder='Hľadaj zastávku...' type='text' id='input'/><div id='searchclear' onmousedown='input.value=&quot;&quot;'><b>x</b></div></div></div><ul><!-- odkazy na zastavky -->"
 url = "http://www.imhd.sk/ba/online-zastavkova-tabula?st="
 
 file1 = open("index_new.html", "w", encoding="utf-8")
@@ -36,7 +36,10 @@ while i < 5000:
         #na linuxe netreba kodovat
         #title = codecs.decode(text, "unicode_escape")
 
-        title = text
+        if text == '':
+            title = 'Najbližšia zastávka'
+        else:
+            title = text
 
         accentless = unidecode.unidecode(title)
 
