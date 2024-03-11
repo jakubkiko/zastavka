e156ee945cee9c642396f1cf42ae3ce468a0480a
Jakub Kiko <jakub.kiko@gmail.com>
2024-03-11 22:54:38 +0100

    fixes

--- zastavka.py
+++ zastavka.py
@@ -5,7 +5,7 @@ import unidecode
 import re
 
 index_html = "<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Zastavka</title><link rel='stylesheet' href='style.css'><script src='search.js' type='text/javascript'/></head><body><nav class='bottom-nav'>         <a href='https://www.nullreferer.com/?https://imhd.sk/ba/planovac-cesty' class='bottom-nav__item'>         <!-- https://www.w3schools.com/charsets/ref_emoji_smileys.asp -->             <b class='material-icons'>&#128654 </b>             Planner         </a>         <a href='https://www.nullreferer.com/?https://ecard.dpb.sk/Account/Login?ReturnUrl=%2FCards' class='bottom-nav__item'>           <b class='material-icons'>&#128179;</b>           Cards         </a>         <a href='https://github.com/jakubkiko/zastavka#' class='bottom-nav__item'>           <b class='material-icons'>&#x2753;</b>           About         </a>       </nav><div id='header'><div class='input'><input placeholder='Hľadaj zastávku...' type='text' id='input'/><div id='searchclear' onmousedown='input.value=&quot;&quot;'><b>x</b></div></div></div><ul><!-- odkazy na zastavky -->"
-url = 'https://www.nullreferer.com/?http://www.imhd.sk/ba/online-zastavkova-tabula?st='
+url = 'https://www.imhd.sk/ba/online-zastavkova-tabula?st='
 
 file1 = open("index_new.html", "w", encoding="utf-8")
 
@@ -47,7 +47,7 @@ while i < 5000:
 
         lowercase = re.sub('[^A-Za-z0-9 ]+', '', lowercase)
 
-        li = '<li><a href="https://www.nullreferer.com/?' + link + '"><strong class="name">' + lowercase + '</strong>' + title \
+        li = '<li><a href="' + link + '"><strong class="name">' + lowercase + '</strong>' + title \
              + '</a></li>'
 
         index_html += li
